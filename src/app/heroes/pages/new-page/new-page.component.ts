47206e6885f63b034e011d955a637dec560be178
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-11 15:42:32 -0600

    Conectar el formulario con el template cada uno de los campos.

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -22,5 +22,12 @@ export class NewPageComponent {
   public plublishers = [
     {id:'DC Comics', desc:'DC - Comics'},
     {id:'Marvel Comics', desc:'Marvel - Comics'},
-  ]
+  ];
+
+  onSubmit():void{
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value,
+    })
+  }
 }
