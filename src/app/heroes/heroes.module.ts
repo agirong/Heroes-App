f2d6502b294fe1dc440c2fa55426408fa93b5358
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-11 09:33:56 -0600

    Autocomplete: Importar ReactiveFormsModule. Crear el html del buscador.

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './components/card/card.component';
 import { HeroPipePipe } from './pipes/hero-pipe.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { HeroPipePipe } from './pipes/hero-pipe.pipe';
     CommonModule,
     HeroesRoutingModule,
     MaterialModule,
+    ReactiveFormsModule,
   ]
 })
 export class HeroesModule { }
