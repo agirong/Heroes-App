ac3e1e2dbe73ed5e45a6c9cbedddcf3e0a43168a
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-14 12:01:54 -0600

    AuthService Servicio de Autenticación.

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environments } from '../../../environments/environments';
+import { User } from '../interfaces/user.interface';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  constructor(private http: HttpClient) { }
+
+  private baseUrl = environments.baseUrl;
+  private user?:User;
+
+  get currentUser():User|undefined{
+    if(!this.user) return undefined;
+
+    return structuredClone(this.user);
+  }
+
+  login(email:string,pass:string):Observable<User>{
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+    .pipe(
+      tap(user=> this.user = user),
+      tap(user=> localStorage.setItem('token',user.id.toString()))
+    )
+  }
+}
