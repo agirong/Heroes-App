5615e94edeae87502c2519d65f32c5e43c16b4ed
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-16 21:56:30 -0600

    Guards de Angular creación del servicio e implementación CanMatch y CanActivate.

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, UrlSegment, GuardResult, MaybeAsync, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanMatch, CanActivate {
+  constructor() { }
+  canMatch(route: Route, segments: UrlSegment[]): boolean| Observable<boolean> {
+    console.log('CanMatch')
+    console.log({route,segments})
+    return true;
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean| Observable<boolean> {
+    console.log('CanActivate')
+    console.log({route,state})
+    return true;
+  }
+
+}
